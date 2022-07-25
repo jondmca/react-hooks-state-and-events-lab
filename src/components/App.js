7a2a54acc083d55170e4308cab57420f9c8a7663
Jonathan Derek McArthur <jondmcarthur@gmail.com>
2022-07-25 17:21:38 -0500

    Completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
 
@@ -6,17 +6,27 @@ function App() {
 
   // replace 'false' with a state variable that can be toggled between true and false
   // this will be used for the Dark Mode Toggle feature
-  const appClass = false ? "App dark" : "App light"
+  const [appClass, setAppClass] = useState(false)
+
+  const [isLight, setIsOn] = useState(true);
+    
+    function handleClick(){
+    console.log("I've been clicked. Teehee!")
+    setIsOn((isLight) => !isLight);
+    setAppClass((appClass) => !appClass);
+  }
+
 
   return (
-    <div className={appClass}>
+    <div className={appClass ? "App dark" : "App light"}>
       <header>
         <h2>Shopster</h2>
-        <button>Dark Mode</button>
+        <button onClick={handleClick}>{isLight ? "Dark Mode" : "Light Mode"}</button>
       </header>
       <ShoppingList items={itemData} />
     </div>
   );
 }
+
 
 export default App;
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+
+  const [addItem, setAddItem] = useState(true);
+  const [innerText, setInnerText] = useState(true);
+  const [buttonColor, setButtonColor] = useState(true);
+
+  function handleAddItem(){
+    console.log("You added an Item!!");
+    setAddItem((addItem) => !addItem);
+    setInnerText((innerText) => !innerText);
+    setButtonColor((buttonColor) => !buttonColor);
+  };
+
   return (
-    <li className="">
+    <li className={addItem ? "" : "in-cart"}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={handleAddItem} className={buttonColor ? "add" : "remove"}>{innerText ? "Add to Cart" : "Remove From Cart"}</button>
     </li>
   );
-}
+};
 
 export default Item;
